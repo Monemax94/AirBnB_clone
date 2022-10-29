3560024b1ec91bea880972a9eccaa4f4a0287b77
Monemax94 <mondayodoh94gmail.com>
2022-10-29 15:05:04 +0100

    Task3-4

---
+++ models/base_model.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""Module for Base class
+Contains the Base class for the AirBnB clone console.
+"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel():
+
+    """ Class for base model object hierarchy.
+    """
+
+    def __init__(self, *args, **kwargs):
+
+        """ Initialization of a Base instance """
+
+        if kwargs is not None and kwargs != {}:
+
+            for key in kwargs:
+
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime
+                    (kwargs["created_at"], "%Y-%m-%dT%H:%M:%S:%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime
+                    (kwargs["update_at"], "%Y-%m-%dT%H:%M:%S:%f")
+
+                else:
+                    self.__dict__[key] = kwargs[key]
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """ Returns a human-readable string representation
+        of an instance or converting an instance to string
+        making it readable """
+
+        return "[{}] ({}) {}".\
+            format(type(self)__name__, self.id, self.__dict__)
+
+    def save(self):
+
+        """ Updates the updated_at attribute with the
+        current date andtime. """
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+
+        """ Returns dictionary representation of an instance. """
+
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
